879fb92bc3e7dfc11053ae55703760e0a07198ca
dmostafiz <dev.mostafiz@gmail.com>
2023-01-12 03:23:05 +0600

    a issue has been solved

--- src/pages/home/sales/invoice.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import InvoiceData from '../../../Components/home/Dashboard/Invoice/InvoiceData'
-
-export default function invoice() {
-  return (
-    <InvoiceData />
-  )
-}
